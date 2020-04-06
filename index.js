d1b2c4a00e979508af34b12f23f6af0dcb71af0f
treverandersen <trever.r.andersen@gmail.com>
2020-04-06 11:26:41 -0500

    completed the 3 labs.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+function saturdayFun(fun="roller-skate") {
+  console.log(`This Saturday, I want to ${fun}!`);
+}
+saturdayFun();
+saturdayFun("go out to eat");
+
+function mondayWork(str="go to the office") {
+  console.log(`This Monday, I will ${str}.`)
+}
+mondayWork();
+mondayWork("go for a run");
+
+function wrapAdjective(w="*") {
+  return function(p="special") {
+    return `You are ${w}${p}${w}.`
+  }
+}
+wrapAdjective()("cool");
+wrapAdjective("%")("a dedicated programmer");
+
